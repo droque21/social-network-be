1a157b85b923ca42901b029ee87d201b1a949e78
David Roque <davidroque21@hotmail.com>
2023-02-11 12:01:12 -0400

    remove .env

---
+++ src/user/infrastructure/delete.controllet.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from 'express';
+import httpStatus from 'http-status';
+import { Controller } from '../../shared/infrastructure/controller/controller';
+import { UserDelete } from '../application/delete.use-case';
+import { UserMongoRepository } from './userMongo.repository';
+
+interface UserDeleteRequest extends Request {
+  params: {
+    id: string
+  }
+}
+
+export class UserDeleteController implements Controller {
+  constructor() { }
+
+  async run(req: UserDeleteRequest, res: Response) {
+    const {
+      id
+    } = req.params;
+
+    const userRepository = new UserMongoRepository();
+    const deleteUserCase = new UserDelete(userRepository);
+    await deleteUserCase.run(id);
+
+    res.status(httpStatus.OK).send({
+      result: "User deleted",
+    });
+  }
+}
