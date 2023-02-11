d893ba778f44d81b63ce50fd8e131a5a31206c30
David Roque <davidroque21@hotmail.com>
2023-02-11 18:57:45 -0400

    remove describe from files

--- src/routes/test/user/useDelete.test.ts
+++ src/routes/test/user/useDelete.test.ts
@@ -1,22 +1,20 @@
 import request from 'supertest'
-import { it, describe, expect } from 'vitest'
+import { expect, it } from 'vitest'
 import { App } from '../../../app'
 import { generateUuid } from '../../../shared/infrastructure/uuid/uuid'
 
-describe('Delete User', () => {
 
-  it('should have a route handler listening to /api/user/:id for delete request', async () => {
-    const response = await request(App.getApp()).delete('/api/user/:id')
-    expect(response.status).not.toBe(404)
-  })
+it('should have a route handler listening to /api/user/:id for delete request', async () => {
+  const response = await request(App.getApp()).delete('/api/user/:id')
+  expect(response.status).not.toBe(404)
+})
 
-  it('should return a 422 status code if the id is invalid', async () => {
-    const response = await request(App.getApp()).delete('/api/user/123')
-    expect(response.status).toBe(422)
-  })
+it('should return a 422 status code if the id is invalid', async () => {
+  const response = await request(App.getApp()).delete('/api/user/123')
+  expect(response.status).toBe(422)
+})
 
-  it('should return a 204 status code if the id is valid', async () => {
-    const response = await request(App.getApp()).delete(`/api/user/${generateUuid()}`)
-    expect(response.status).toBe(204)
-  })
+it('should return a 204 status code if the id is valid', async () => {
+  const response = await request(App.getApp()).delete(`/api/user/${generateUuid()}`)
+  expect(response.status).toBe(204)
 })
